ce4b405cd320ede496048f8441797f95470b0370
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2021-02-23 21:30:50 +0200

    last commit

---
+++ WatchStore/WatchStore/ApplicationViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchStore
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Watch selectedPhone;
+
+        public ObservableCollection<Watch> Phones { get; set; }
+        public Watch SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Watch>
+            {
+                new Watch { Title="A168WEM-7VT", Company = "Casio", Price = 955 },
+                new Watch {Title="Watch Series 3", Company="Apple", Price = 4500 },
+                new Watch {Title="Pro Diver", Company="Rolex", Price=280000 },
+                new Watch {Title="Seamaster", Company="Omega", Price=48000 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
