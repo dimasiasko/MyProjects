cba216511095990829b1a0f60ff0e547aea48dbe
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2021-02-23 18:20:17 +0200

    last commit

---
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            WebClient wb = new WebClient();
+            String Response = wb.DownloadString("https://sinoptik.ua/погода-вараш");
+            String Rate =
+                System.Text.RegularExpressions.Regex.Match(Response, @"<td class=""p5 "" >""([0-9]+)""&deg;</td>")
+                    .Groups[1].Value;
+
+            txtCurrentTemperature.Text = Rate + " °C";
+
+
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
