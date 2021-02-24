183798a56cb0211b9fc2ecbe5e7c83fdc9a5ffca
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2021-02-24 13:10:53 +0200

    last commit

---
+++ ATM/ATM/TakeCashWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATM
+{
+    /// <summary>
+    /// Логика взаимодействия для TakeCashWindow.xaml
+    /// </summary>
+    public partial class TakeCashWindow : Window
+    {
+        public TakeCashWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnToConfirm_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
