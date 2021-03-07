1c1b975be5439c4f07b577b160adb7c0b838eef3
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2021-03-07 16:52:22 +0200

    init

---
+++ StowwatchMVP/StowwatchMVP/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StowwatchMVP
+{
+    class Model
+    {
+        internal int s;
+        public string Time()
+        {
+            s += 1;
+            return (s >= 60) ? "min. " + s / 60 + " s. " + s % 60 : s.ToString();
+        }
+        public void Clear()
+        {
+            s = 0;
+        }
+    }
+}
